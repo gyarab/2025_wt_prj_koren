6c3ed377180ef5445e69d3eaa1161528af2c6220
Antonín Kořen <antonin.koren.s@gyarab.cz>
2026-05-10 21:42:11 +0200

    Replace movie endpoints with car endpoints

--- api.py
+++ api.py
@@ -3,118 +3,55 @@ from typing import List
 from django.shortcuts import get_object_or_404
 from ninja import NinjaAPI, ModelSchema
 
-from .models import Movie, Comment
+from .models import Car
 
 
 api = NinjaAPI()
 
 
-# =========================
-# SCHÉMATA PRO MOVIE
-# =========================
-
-class MovieOut(ModelSchema):
+class CarOut(ModelSchema):
     class Config:
-        model = Movie
-        model_fields = ["id", "title", "description", "year"]
+        model = Car
+        model_fields = ["id", "brand", "model", "year", "price"]
 
 
-class MovieIn(ModelSchema):
+class CarIn(ModelSchema):
     class Config:
-        model = Movie
-        model_fields = ["title", "description", "year"]
+        model = Car
+        model_fields = ["brand", "model", "year", "price"]
 
 
-# =========================
-# SCHÉMATA PRO COMMENT
-# =========================
-
-class CommentOut(ModelSchema):
-    class Config:
-        model = Comment
-        model_fields = ["id", "movie", "author", "text"]
+@api.get("/car", response=List[CarOut])
+def list_cars(request):
+    return Car.objects.all()
 
 
-class CommentIn(ModelSchema):
-    class Config:
-        model = Comment
-        model_fields = ["movie", "author", "text"]
+@api.get("/car/{car_id}", response=CarOut)
+def get_car(request, car_id: int):
+    return get_object_or_404(Car, id=car_id)
 
 
-# =========================
-# MOVIE ENDPOINTY
-# =========================
-
-@api.get("/movie", response=List[MovieOut])
-def list_movies(request):
-    return Movie.objects.all()
+@api.post("/car", response=CarOut)
+def create_car(request, data: CarIn):
+    car = Car.objects.create(**data.dict())
+    return car
 
 
-@api.get("/movie/{movie_id}", response=MovieOut)
-def get_movie(request, movie_id: int):
-    return get_object_or_404(Movie, id=movie_id)
-
-
-@api.post("/movie", response=MovieOut)
-def create_movie(request, data: MovieIn):
-    movie = Movie.objects.create(**data.dict())
-    return movie
-
-
-@api.put("/movie/{movie_id}", response=MovieOut)
-def update_movie(request, movie_id: int, data: MovieIn):
-    movie = get_object_or_404(Movie, id=movie_id)
+@api.put("/car/{car_id}", response=CarOut)
+def update_car(request, car_id: int, data: CarIn):
+    car = get_object_or_404(Car, id=car_id)
 
     for attr, value in data.dict().items():
-        setattr(movie, attr, value)
+        setattr(car, attr, value)
 
-    movie.save()
-    return movie
-
-
-@api.delete("/movie/{movie_id}")
-def delete_movie(request, movie_id: int):
-    movie = get_object_or_404(Movie, id=movie_id)
-    movie.delete()
-
-    return {"success": True}
+    car.save()
+    return car
 
 
-# =========================
-# COMMENT ENDPOINTY
-# dobrovolné rozšíření
-# =========================
-
-@api.get("/comment", response=List[CommentOut])
-def list_comments(request):
-    return Comment.objects.all()
-
-
-@api.get("/comment/{comment_id}", response=CommentOut)
-def get_comment(request, comment_id: int):
-    return get_object_or_404(Comment, id=comment_id)
-
-
-@api.post("/comment", response=CommentOut)
-def create_comment(request, data: CommentIn):
-    comment = Comment.objects.create(**data.dict())
-    return comment
-
-
-@api.put("/comment/{comment_id}", response=CommentOut)
-def update_comment(request, comment_id: int, data: CommentIn):
-    comment = get_object_or_404(Comment, id=comment_id)
-
-    for attr, value in data.dict().items():
-        setattr(comment, attr, value)
-
-    comment.save()
-    return comment
-
-
-@api.delete("/comment/{comment_id}")
-def delete_comment(request, comment_id: int):
-    comment = get_object_or_404(Comment, id=comment_id)
-    comment.delete()
+@api.delete("/car/{car_id}")
+def delete_car(request, car_id: int):
+    car = get_object_or_404(Car, id=car_id)
+    car.delete()
 
     return {"success": True}
+    
