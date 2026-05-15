a27eda0172e432e06d1b6522a8204bec8bbba65d
antoninkoren <antonin.koren.s@gyarab.cz>
2026-05-15 09:46:04 +0200

    Add API endpoints for car management and update settings for template directory

--- prj/app/settings.py
+++ prj/app/settings.py
@@ -34,11 +34,10 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -46,6 +45,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
--- prj/app/urls.py
+++ prj/app/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from app.views import render_home, render_about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # If you have a chat app, you'll eventually add: path('', include('chat.urls')),
+    path('', render_home, name='home'),
+    path('about/', render_about, name='about'),
 ]
