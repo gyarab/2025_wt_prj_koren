4c2251f419b21a92b59a14210ee6181822121d56
antoninkoren <antonin.koren.s@gyarab.cz>
2026-06-05 08:36:53 +0200

    Fix package.json script formatting and add initial app.js and main.js files

---
+++ frontend/src/app.js
@@ -0,0 +1,15 @@
+import { RouterLink } from "vue-router";
+
+<script setup>
+    import { RouterLink } from "vue";
+</script>
+
+<template>
+    <header>
+        <RouterLink to="/">
+        <h1>WT Project</h1>
+    </header>
+    <main>
+        <RouterWiev />
+    </main>
+</template>
